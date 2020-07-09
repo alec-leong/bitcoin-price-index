84e631c11606fec73d47faf7729df82eb0fedbae
Alec Leong <leongalec@gmail.com>
2020-07-09 09:47:01 -0700

    <App /> renders <Clock />, <Canvas />, and <Disclaimer />.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Canvas from './Canvas';
+import Clock from './Clock';
+import Disclaimer from './Disclaimer';
+
+const App = () => (
+  <div>
+    <Clock />
+    <Canvas />
+    <Disclaimer />
+  </div>
+);
+
+export default App;
