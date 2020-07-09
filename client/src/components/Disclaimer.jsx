ee47f3e19431defc2f8f0b6d953aa4c9e39a5a00
Alec Leong <leongalec@gmail.com>
2020-07-09 09:51:05 -0700

    <Disclaimer /> renders a <p> element citing data source credit to CoinDesk.

---
+++ client/src/components/Disclaimer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Disclaimer = () => (
+  <p>
+    Disclaimer: This data was produced from the&nbsp;
+    <a href="https://www.coindesk.com/coindesk-api">CoinDesk Bitcoin Price Index</a>
+    .
+  </p>
+);
+
+export default Disclaimer;
