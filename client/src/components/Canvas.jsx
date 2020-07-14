8b51c25109cdaa545017d279d3d61ba209b721f4
Alec Leong <leongalec@gmail.com>
2020-07-13 22:59:47 -0700

    Updated <Canvas /> to render newly formated data (prev: object --> now: array) sent from app server.

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Chart from 'chart.js';
+import moment from 'moment';
 import React, { Component, createRef } from 'react';
 
 class Canvas extends Component {
@@ -17,6 +18,16 @@ class Canvas extends Component {
         violet: '#EE82EE',
         black: '#000000',
       },
+      marketSummaryTimes: {
+        '1 day': 2,
+        '5 days': 5,
+        '1 month': 30,
+        '6 months': 180,
+        YTD: Math.abs(Date.parse(moment.utc().format('YYYY-MM-DD')) - Date.parse(`${moment.utc().format('YYYY')}-01-01`)) * 10 ** -3 * (1 / 60) * (1 / 60) * (1 / 24), // year to date (YTD) in number of days
+        '1 year': 365,
+        '5 years': 365 * 5,
+        Max: Math.abs(Date.parse(moment.utc().format('YYYY-MM-DD')) - Date.parse(moment('2013-09-01').format('YYYY-MM-DD'))) * 10 ** -3 * (1 / 60) * (1 / 60) * (1 / 24),
+      },
     };
 
     this.ref = createRef();
@@ -26,14 +37,16 @@ class Canvas extends Component {
     try {
       const response = await axios.get('/bpi');
       const { data } = response;
+
+      const { marketSummaryTimes } = this.state;
       const { colors } = this.state;
       const colorCodes = Object.values(colors);
 
-      const datasets = Object.entries(data).reduce((accumulator, [label, points], index) => {
+      const datasets = Object.entries(marketSummaryTimes).reduce((accumulator, [summary, numDays], index) => {
         accumulator.push({
-          label,
+          label: summary,
           borderColor: colorCodes[index],
-          data: points,
+          data: data.slice(data.length - numDays, data.length),
           pointBackgroundColor: colorCodes[index],
           fill: false,
           showLine: true,
